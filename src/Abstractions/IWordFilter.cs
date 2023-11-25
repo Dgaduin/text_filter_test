7c7293d391239080245399987625a9304fc66d66
Atanas Pashkov <dgaduin@gmail.com>
2023-11-25 22:10:20 +0000

    Move the docs in the right place

--- src/Abstractions/IWordFilter.cs
+++ src/Abstractions/IWordFilter.cs
@@ -2,5 +2,10 @@
 
 public interface IWordFilter
 {
+    /// <summary>
+    /// Tests if a word matches a filter
+    /// </summary>
+    /// <param name="word">word to test</param>
+    /// <returns>true if the word passes the filter, false if it should be filtered out</returns>
     bool TestWord(string word);
 }
--- src/Abstractions/IWordFilterWithOptions.cs
+++ src/Abstractions/IWordFilterWithOptions.cs
@@ -3,9 +3,4 @@ namespace TextFilter.Abstractions;
 public interface IWordFilterWithOptions<T> : IWordFilter where T : IWordFilterOptions
 {
     T Options { get; }
-    /// <summary>
-    /// Tests if a word matches a filter
-    /// </summary>
-    /// <param name="word">word to test</param>
-    /// <returns>true if the word passes the filter, false if it should be filtered out</returns>
 }
