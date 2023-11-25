00988d8a4e4e2bd8959b5871ea34d629eb4dbbad
Atanas Pashkov <dgaduin@gmail.com>
2023-11-25 21:27:31 +0000

    Expand the docs

--- src/Abstractions/IWordFilter.cs
+++ src/Abstractions/IWordFilter.cs
@@ -7,6 +7,6 @@ public interface IWordFilter<T> where T : IWordFilterOptions
     /// Tests if a word matches a filter
     /// </summary>
     /// <param name="word">word to test</param>
-    /// <returns>Returns true if the word passes the filter</returns>
+    /// <returns>true if the word passes the filter, false if it should be filtered out</returns>
     bool TestWord(string word);
 }
