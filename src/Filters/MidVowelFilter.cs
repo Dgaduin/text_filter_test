690cf5d04f566398744b46338399af175e0a9c50
Atanas Pashkov <dgaduin@gmail.com>
2023-11-25 21:27:59 +0000

    Implement the logic for the mid vowel filter

--- src/Filters/MidVowelFilter.cs
+++ src/Filters/MidVowelFilter.cs
@@ -4,10 +4,18 @@ namespace TextFilter.Filters;
 
 public class MidVowelFilter(MidVowelFilterOptions options) : IWordFilter<MidVowelFilterOptions>
 {
+    private static readonly char[] vowels = ['a', 'e', 'i', 'o', 'u'];
     public MidVowelFilterOptions Options { get; init; } = options;
 
     public bool TestWord(string word)
     {
-        throw new NotImplementedException();
+        var oddNumberOfChars = word.Length % 2 != 0;
+        // not exact, but helps with offsetting the array indexing
+        var middle = word.Length / 2;
+
+        if (oddNumberOfChars)
+            return !vowels.Contains(word[middle]);
+        else
+            return !(vowels.Contains(word[middle - 1]) || vowels.Contains(word[middle]));
     }
 }
